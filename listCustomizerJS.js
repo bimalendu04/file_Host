0519670d66330f9139e7b5e71b6c7c2bb6344d51
Bimalendu sarkar <28942300+bimalendu04@users.noreply.github.com>
2019-10-24 11:21:42 +0530

    added console

--- listCustomizerJS.js
+++ listCustomizerJS.js
@@ -2,10 +2,14 @@ var elements = document.getElementsByClassName('parentClass');
 
 var elementsArr = Array.prototype.slice.call( elements );
 
+console.log('elementsArr', elementsArr);
 if (elementsArr && elementsArr.length) {
 	elementsArr.map((element) => {
+        console.log('elements map', element);
 		element.addEventListener("click", () => {
-			element.firstElementChild.setAttribute("style", "display: block;");
+            element.firstElementChild.setAttribute("style", "display: block;");
+            
+            console.log('elements map2', element);
 		  });
 	});
 }
