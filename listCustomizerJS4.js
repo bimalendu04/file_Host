79019a0592f82f2deab40c6e66f4c4a4e1c782ef
Bimalendu sarkar <28942300+bimalendu04@users.noreply.github.com>
2019-10-24 17:25:42 +0530

    Add files via upload

---
+++ listCustomizerJS4.js
@@ -0,0 +1,24 @@
+var elements = document.getElementsByClassName('clickableTitle');
+
+var elementsArr = Array.prototype.slice.call( elements );
+
+console.log('elementsArr', elementsArr);
+if (elementsArr && elementsArr.length) {
+	elementsArr.map((element) => {
+        console.log('elements map', element);
+        element.nextSibling.style.transition = "all 0.5s";
+		element.addEventListener("click", () => {
+            if (element.nextSibling.style.height == "" || element.nextSibling.style.height == "auto") {
+                element.style.borderBottom = "none";
+                element.children[0].style.transform = "rotate(90deg)";
+                element.nextSibling.style.height = "0";
+            } else {
+                element.style.borderBottom = "1px solid #f2f2f2";
+                element.children[0].style.transform = "rotate(90deg)";
+                element.nextSibling.style.height = "auto";
+            }
+            
+            console.log('elements map2', element);
+		  });
+	});
+}
