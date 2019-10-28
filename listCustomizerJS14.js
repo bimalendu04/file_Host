cf306e8826f192e43e569bc8034c7442468033c8
Bimalendu sarkar <28942300+bimalendu04@users.noreply.github.com>
2019-10-28 13:10:27 +0530

    Add files via upload

---
+++ listCustomizerJS14.js
@@ -0,0 +1,40 @@
+var elements = document.getElementsByClassName('clickableTitle');
+
+var elementsArr = Array.prototype.slice.call(elements);
+
+if (elementsArr && elementsArr.length) {
+	elementsArr.map((element, index) => {
+		element.nextSibling.style.transition = "all 0.5s";
+		element.style.transition = "all 0.5s";
+		element.addEventListener("click", () => {
+			debugger;
+			if (element.nextSibling.style.height == "" || element.nextSibling.style.height == "auto") {
+				element.style.borderBottom = "none";
+				element.children[0].style.transform = "rotate(0deg)";
+				// element.nextSibling.style.display = "none";
+				element.nextSibling.style.paddingTop = "0px";
+				element.nextSibling.style.paddingBottom = "0px";
+				element.nextSibling.style.marginTop = "0px";
+				element.nextSibling.style.height = "0px";
+			} else {
+				element.style.borderBottom = "1px solid #f2f2f2";
+				element.children[0].style.transform = "rotate(90deg)";
+				// element.nextSibling.style.display = "block";
+				element.nextSibling.style.height = "auto";
+				element.nextSibling.style.paddingBottom = "10px";
+				element.nextSibling.style.marginTop = "20px";
+				elementsArr.map((otherElement, otherIndex) => {
+					if (otherIndex !== index) {
+						otherElement.style.borderBottom = "none";
+						otherElement.children[0].style.transform = "rotate(0deg)";
+						// otherElement.nextSibling.style.display = "none";
+						element.nextSibling.style.paddingTop = "0px";
+						element.nextSibling.style.paddingBottom = "0px";
+						element.nextSibling.style.marginTop = "0px";
+						element.nextSibling.style.height = "0px";
+					}
+				});
+			}
+		});
+	});
+}
